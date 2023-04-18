f35d95ac52f6bb27fa650ea843df4368d4df11ce
JustTrzcina <andrzej.maciaszek009@gmail.com>
2023-04-18 18:59:03 +0200

    finish quiz-oop project

---
+++ Intermediate/quiz-oop/data.py
@@ -0,0 +1,102 @@
+question_data = [
+    {
+        "category": "History",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The Cold War ended with Joseph Stalin&#039;s death.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+                "True"
+        ]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Deus Ex (2000) does not feature the World Trade Center because it was destroyed by terrorist attacks according to the game&#039;s plot.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Gordon Freeman, the protagonist of &quot;Half-Life&quot;, is said to have once had a ponytail.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The end credits sequence in Grand Theft Auto 5 is over half an hour long.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The Windows ME operating system was released in the year 2000.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In Pok&eacute;mon, Bulbasaur is the only starter pokemon that is a Grass/Poison type.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    },
+    {
+        "category": "Geography",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Tokyo is the capital of Japan.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    },
+    {
+        "category": "Entertainment: Television",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In &quot;Star Trek&quot;, Klingons are aliens.",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    },
+    {
+        "category": "Geography",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "Hungary is the only country in the world beginning with H.",
+        "correct_answer": "False",
+        "incorrect_answers": [
+                "True"
+        ]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The game &quot;Pocket Morty&quot; has a Morty called &quot;Pocket Mortys Morty&quot;?",
+        "correct_answer": "True",
+        "incorrect_answers": [
+                "False"
+        ]
+    }
+]
